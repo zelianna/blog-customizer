660e814951b0325fb5d2081ec25ea5e9deb6547d
Anna Zelenskaia <anya034@yandex.ru>
2024-11-20 20:56:21 +0100

    Pr10_ArrowButton_click

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,40 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const toggleSidebar = () => {
+		console.log('>>>>>>>>here');
+		console.log(isOpen);
+		setIsOpen((prev) => !prev);
+		console.log(isOpen);
+	};
+	const handleClickOutside = (event: MouseEvent) => {
+		if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+			setIsOpen(false);
+		}
+	};
+	useEffect(() => {
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={toggleSidebar} />
+			<aside ref={containerRef} 
+				   className={`${styles.container} ${isOpen ? styles.open : ''}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
