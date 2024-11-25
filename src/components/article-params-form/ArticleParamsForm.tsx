18fdf2d420f2d4b8c05408a79c17c88942be70cc
Anna Zelenskaia <anya034@yandex.ru>
2024-11-25 21:16:36 +0100

    Pr10_itog2

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import {Text} from 'src/ui/text';
+import { Text } from 'src/ui/text';
 import { Separator } from 'src/ui/separator';
 import { Select } from 'src/ui/select';
-import {RadioGroup} from 'src/ui/radio-group';
+import { RadioGroup } from 'src/ui/radio-group';
 import {
 	backgroundColors,
 	contentWidthArr,
@@ -12,79 +12,91 @@ import {
 	fontColors,
 	fontFamilyOptions,
 	fontSizeOptions,
-	ArticleStateType
+	ArticleStateType,
 } from 'src/constants/articleProps';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
-
 
 import styles from './ArticleParamsForm.module.scss';
 interface ArticleParamsFormProps {
 	initialState: ArticleStateType;
 	onApply: (newState: ArticleStateType) => void;
 	onReset: () => void;
-  }
-  
-export const ArticleParamsForm:React.FC<ArticleParamsFormProps>  = ({ initialState, onApply, onReset }) => {
+}
+
+export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({
+	initialState,
+	onApply,
+	onReset,
+}) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [formState, setFormState] = useState<ArticleStateType>({ ...defaultArticleState });
-    
-	const containerRef = useRef<HTMLDivElement | null>(null);
-	useOutsideClickClose({
-		isOpen, 
-		rootRef: containerRef, 
-		onChange: (newValue) => setIsOpen(newValue)
+	const [formState, setFormState] = useState<ArticleStateType>({
+		...defaultArticleState,
 	});
 
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	useOutsideClickClose({
+		isOpen,
+		rootRef: containerRef,
+		onChange: (newValue) => setIsOpen(newValue),
+	});
 
 	const toggleSidebar = () => {
 		setIsOpen((prev) => !prev);
 	};
 	useEffect(() => {
 		setFormState({ ...initialState });
-	  }, [initialState]);
+	}, [initialState]);
 
 	const handleApply = () => {
 		onApply(formState);
-		setIsOpen(false); 
+		setIsOpen(false);
 	};
-
 
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={toggleSidebar} />
-			<aside ref={containerRef} 
-				   className={`${styles.container} ${isOpen ? styles.container_open : ''}`}>	
+			<aside
+				ref={containerRef}
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
-				    <Text as='h1' size={31} weight={800} uppercase>
+					<Text as='h1' size={31} weight={800} uppercase>
 						Задайте параметры
 					</Text>
-                    <Select
+					<Select
 						title='Шрифт'
 						options={fontFamilyOptions}
 						selected={formState.fontFamilyOption}
-						onChange={(newValue) => setFormState((prevState) => ({
-							...prevState,
-							fontFamilyOption: newValue,
-						}))}					
+						onChange={(newValue) =>
+							setFormState((prevState) => ({
+								...prevState,
+								fontFamilyOption: newValue,
+							}))
+						}
 					/>
 					<RadioGroup
 						title='Размер шрифта'
 						name='fontSize'
 						options={fontSizeOptions}
 						selected={formState.fontSizeOption}
-						onChange={(newValue) => setFormState((prevState) => ({
-							...prevState,
-							fontSizeOption: newValue,
-						}))}					
-                    />
+						onChange={(newValue) =>
+							setFormState((prevState) => ({
+								...prevState,
+								fontSizeOption: newValue,
+							}))
+						}
+					/>
 					<Select
 						title='Цвет шрифта'
 						options={fontColors}
 						selected={formState.fontColor}
-						onChange={(newValue) => setFormState((prevState) => ({
-							...prevState,
-							fontColor: newValue,
-						}))}					
+						onChange={(newValue) =>
+							setFormState((prevState) => ({
+								...prevState,
+								fontColor: newValue,
+							}))
+						}
 					/>
 
 					<Separator></Separator>
@@ -93,24 +105,37 @@ export const ArticleParamsForm:React.FC<ArticleParamsFormProps>  = ({ initialSta
 						title='Цвет фона'
 						options={backgroundColors}
 						selected={formState.backgroundColor}
-						onChange={(newValue) => setFormState((prevState) => ({
-							...prevState,
-							backgroundColor: newValue,
-						}))}					
+						onChange={(newValue) =>
+							setFormState((prevState) => ({
+								...prevState,
+								backgroundColor: newValue,
+							}))
+						}
 					/>
 					<Select
 						title='Ширина контента'
 						options={contentWidthArr}
 						selected={formState.contentWidth}
-						onChange={(newValue) => setFormState((prevState) => ({
-							...prevState,
-							contentWidth: newValue,
-						}))}
-					
+						onChange={(newValue) =>
+							setFormState((prevState) => ({
+								...prevState,
+								contentWidth: newValue,
+							}))
+						}
 					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' onClick={onReset} />
-						<Button title='Применить' htmlType='button' type='apply' onClick={handleApply}  />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={onReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='button'
+							type='apply'
+							onClick={handleApply}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {ArticleStateType, defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,7 +16,9 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [pageState, setPageState] = useState<ArticleStateType>({ ...defaultArticleState });
+	const [pageState, setPageState] = useState<ArticleStateType>({
+		...defaultArticleState,
+	});
 
 	return (
 		<main
